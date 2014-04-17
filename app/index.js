887efb842c23d852ad4be8c6c3a4cf0f3d298831
vkkadian <vkkadian@yahoo.com>
2014-04-17 23:57:15 +0530

    verifying travis build through this commit
    Just a rudimentary change from filria to FILRIA

--- app/index.js
+++ app/index.js
@@ -23,7 +23,7 @@ var FilriaGenerator = yeoman.generators.Base.extend({
     this.log(this.yeoman);
 
     // replace it with a short and sweet description of your generator
-    this.log(chalk.magenta('You\'re using the fantastic Filria generator.'));
+    this.log(chalk.magenta('You\'re using the fantastic FILRIA generator.'));
 
     var prompts = [{
       type: 'confirm',
